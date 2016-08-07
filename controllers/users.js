4d442189b8c6e1eaa66a9a2f6a0c75df7efdd0d9
apenchan <a.penchansky@gmail.com>
2016-08-07 16:12:36 +0300

    added throw error

--- controllers/users.js
+++ controllers/users.js
@@ -23,11 +23,11 @@ router.post('/register', function (req, res){
 router.get('/search', function(req,res){   
 	// var schoolQuery = req.body.result;
 	console.log(req.body);
-	// deepPrint(ingredientQuery);
 	var sat25 = req.query.sat25;
 	var act25 = req.query.act25;
 	console.log(req.query) 
   request("https://www.nearbycolleges.info/api/search?key=" + process.env.NEARBY_COLLEGES_ID + "&filter=tests.act25%3A>" + act25 + "%2Ctests.sat25%3A>" + sat25, function(error, response, body){
+  	if(error) throw error
     if(!error && response.statusCode == 200){
     	result = JSON.parse(body);
     	res.send({result});
